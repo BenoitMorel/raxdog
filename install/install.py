10b71f2b0ddc85f54b1f12d750bfa95c6c3ef107
BenoitMorel <benomorel@gmail.com>
2017-12-13 14:51:58 +0100

    boost and bio++ installation python scripts

--- install/install.py
+++ install/install.py
@@ -3,6 +3,13 @@ import sys
 import subprocess
 import raxdog_install_constants as const
 
+def mkdirnocheck(path):
+  try:
+    os.mkdir(path)
+  except OSError:
+    pass
+
+
 def build_raxml():
   os.chdir(const.RAXML_PATH)
   try:
@@ -20,8 +27,19 @@ def build_boost():
   subprocess.check_call(["./b2", "headers", "--with-mpi", "--with-serialization", "--with-graph"])
   subprocess.check_call(["./b2", "--with-mpi", "--with-serialization", "--with-graph"])
 
+def build_bpp(package):
+  os.chdir(os.path.join(const.BPP_PATH, "bpp-" + package))
+  mkdirnocheck("build")
+  os.chdir("build")
+  subprocess.check_call(["cmake", "-DCMAKE_INSTALL_PREFIX=" + const.BPP_INSTALL_PATH, ".."])
+  subprocess.check_call(["make"])
+  subprocess.check_call(["make", "install"])
+  
 #build_raxml()
 #build_boost()
+build_bpp("core")
+build_bpp("seq")
+build_bpp("phyl")
 
 
 
--- install/raxdog_install_constants.py
+++ install/raxdog_install_constants.py
@@ -3,6 +3,8 @@ import os
 RAXDOG_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 RAXML_PATH= os.path.join(RAXDOG_ROOT, "raxml-ng")
 BOOST_PATH= os.path.join(RAXDOG_ROOT, "boost")
+BPP_PATH= os.path.join(RAXDOG_ROOT, "bpp")
+BPP_INSTALL_PATH = os.path.join(BPP_PATH, "bppinstall")
 
 
 
