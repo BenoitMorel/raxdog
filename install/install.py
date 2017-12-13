acbd9de98442cadacc5a672e3892fdfdc185a8d4
BenoitMorel <benomorel@gmail.com>
2017-12-13 13:34:09 +0100

    Boost installation script

--- install/install.py
+++ install/install.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 import raxdog_install_constants as const
 
-def install_raxml():
+def build_raxml():
   os.chdir(const.RAXML_PATH)
   try:
     os.mkdir("build")
@@ -13,6 +13,15 @@ def install_raxml():
   subprocess.check_call(["cmake", '-DUSE_MPI=ON', ".."])
   subprocess.check_call(["make"])
 
+def build_boost():
+  os.chdir(const.BOOST_PATH)
+  #todobenoit: this is not portable
+  subprocess.check_call(["./bootstrap.sh"])
+  subprocess.check_call(["./b2", "headers", "--with-mpi", "--with-serialization", "--with-graph"])
+  subprocess.check_call(["./b2", "--with-mpi", "--with-serialization", "--with-graph"])
+
+#build_raxml()
+#build_boost()
 
 
-install_raxml()
+
--- install/raxdog_install_constants.py
+++ install/raxdog_install_constants.py
@@ -2,6 +2,7 @@ import os
 
 RAXDOG_ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 RAXML_PATH= os.path.join(RAXDOG_ROOT, "raxml-ng")
+BOOST_PATH= os.path.join(RAXDOG_ROOT, "boost")
 
 
 
